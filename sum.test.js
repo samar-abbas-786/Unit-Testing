3ab567c36ae1eab5e59e32e8436c0a2dca23705c
Samar Abbas <samarabbas172003@gmail.com>
2024-07-21 16:40:18 +0530

    test2

--- sum.test.js
+++ sum.test.js
@@ -2,3 +2,11 @@ const sum = require("./index");
 test("add 1 and 2", () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test("sum is not equal to 0", () => {
+  for (let i = 1; i < 10; i++) {
+    for (let j = 1; j < 10; j++) {
+      expect(sum(i,j)).not.toBe(0);
+    }
+  }
+});
